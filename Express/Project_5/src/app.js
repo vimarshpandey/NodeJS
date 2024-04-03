eb42e3fb47ba02234a8a1fd82afcd906065792b9
Vimarsh Pandey <vimarsh240501@gmail.com>
2024-04-03 18:08:01 +0530

    path correction

--- Express/Project_5/src/app.js
+++ Express/Project_5/src/app.js
@@ -7,9 +7,9 @@ const app = express();
 
 const port = process.env.PORT||3000;
 
-const publicDirectoryPath = path.join(__dirname, '/public');
-const viewPath = path.join(__dirname, '/templet/views');
-const partialPath = path.join(__dirname, '/templet/partials');
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewPath = path.join(__dirname, '../templet/views');
+const partialPath = path.join(__dirname, '../templet/partials');
 
 app.set('view engine', 'hbs');
 app.set('views', viewPath);
